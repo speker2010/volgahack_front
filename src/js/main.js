ce5da6149fdf1c1561f3a823fbf3eb43e1f66663
Artem Besedin <speker@mail.ru>
2017-10-10 18:17:47 +0400

    jquery.maskedinput was replaced by jquery.inputmask

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
     console.log('hello world');
-    $('[type=tel]').mask('+7 (999) 999 99 99');
+    $('[type=tel]').inputmask({
+            mask: '+7(999)999 99 99'
+        });
 });
