98abe48fa5ba4ad623e359acecefb932a4f48d9e
Artem Besedin <speker@mail.ru>
2018-09-30 00:32:37 +0400

    Add bootstrap material. Add vue. Add ajax for sign-up, tickets and sign-in

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,108 @@ $(document).ready(function () {
     var ticketsApp = new Vue({
         el: '#tickets',
         data: {
-            tickets: [
-                { title: 'Титле 1', description: 'описание 1', price: '500' }
-            ]
+            tickets: [],
+            error: ''
+        },
+        mounted() {
+            var _this = this;
+            $.ajax({
+                url: '/tasks.json',
+                method: 'GET',
+                type: 'json',
+                success: function (response) {
+                    if (response.result) {
+                        var tickets = response.data;
+                        for (index in tickets) {
+                            var ticket = tickets[index];
+                            console.log(ticket.description)
+                            ticket.description = markdown.toHTML(ticket.description);
+                            _this.tickets.push(tickets[index]);
+                        }
+                    }
+                },
+                error: function (response) {
+                    if (response.data) {
+                        _this.error = e.data.error;
+                    }
+                }
+            })
         }
     });
+
+    var signIn = new Vue({
+        el: '#sign-in',
+        data: {
+            username: '',
+            password: '',
+            rememberMe: ''
+        },
+        methods: {
+            submit: function (e) {
+                e.preventDefault();
+                var _this = this;
+
+                $.ajax({
+                    url: '/sign-in.json',
+                    data: {
+                       username: _this.username,
+                       password: _this.password,
+                       "remember-me": _this.rememberMe
+                    },
+                    method: 'GET',
+                    type: 'json',
+                    success: function (response) {
+                        if (response) {
+                            console.log(repsonse, 'success');
+                        }
+                    },
+                    error: function (response) {
+                        if (response) {
+                            console.log(response, 'error');
+                        }
+                    }
+                });
+            }
+        }
+    });
+
+    var signUpApp = new Vue({
+       el: '#sign-up',
+       data: {
+           username: '',
+           password: '',
+           passwordRepeat: '',
+           errorName: '',
+           errorPass: ''
+       },
+        methods: {
+           submit: function (e) {
+               e.preventDefault();
+               console.log(this.username);
+               console.log(this.password);
+               var _this = this;
+               $.ajax({
+                   //url: 'http://192.168.1.128:7000/login',
+                   url: '/sign-up.json',
+                   data: {
+                       username: _this.username,
+                       password: _this.password,
+                       passwordRepeat: _this.passwordRepeat
+                       /*"remember-me": _this.rememberMe*/
+                   },
+                   method: 'GET',
+                   success: function (e) {
+                       console.log(e, 'success');
+                   },
+                   error: function (e) {
+                       console.log(e, 'error');
+                   }
+               });
+           }
+        }
+    });
+
+
 
     var app = new Vue({
         el: '#app',
@@ -50,3 +147,13 @@ function getUrlParameters(parameter, staticURL, decode) {
 
 var idParameter = getUrlParameters("id", "", true);
 console.log(idParameter);
+
+function bAjax(url, data, method, successCallback, errorCallback) {
+    $.ajax({
+        url: url,
+        data: data,
+        method: method,
+        success: successCallback,
+        error: errorCallback
+    });
+}
