e9938f51055adc10a36974bcdf41fe7a418b0ae8
Artem Besedin <speker@mail.ru>
2018-09-29 18:51:38 +0400

    Add vue. Add vue for tickets list

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
     console.log('hello world');
+    var ticketsApp = new Vue({
+        el: '#tickets',
+        data: {
+            tickets: []
+        }
+    });
+
+    var ticketApp
+
+    var app = new Vue({
+        el: '#app',
+        data: {
+            hello: 'Привет, Vue!'
+        }
+    });
 
 /*    var markdownText = "Hello.\\n\\n* This is markdown.\\n* It is fun\\n* Love it or leave it.";
     console.log(markdown);
