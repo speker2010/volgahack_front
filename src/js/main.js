a415735b31b6810cd3013645f66aec2175a6d8c7
Artem Besedin <speker@mail.ru>
2018-09-30 01:38:41 +0400

    Add ajax to tickets, users. Refactor vue code.

--- src/js/main.js
+++ src/js/main.js
@@ -1,129 +1,168 @@
 $(document).ready(function () {
     console.log('hello world');
-    var ticketsApp = new Vue({
-        el: '#tickets',
-        data: {
-            tickets: [],
-            error: ''
-        },
-        mounted() {
-            var _this = this;
-            $.ajax({
-                url: '/tasks.json',
-                method: 'GET',
-                type: 'json',
-                success: function (response) {
-                    if (response.result) {
-                        var tickets = response.data;
-                        for (index in tickets) {
-                            var ticket = tickets[index];
-                            console.log(ticket.description)
-                            ticket.description = markdown.toHTML(ticket.description);
-                            _this.tickets.push(tickets[index]);
-                        }
-                    }
-                },
-                error: function (response) {
-                    if (response.data) {
-                        _this.error = e.data.error;
-                    }
-                }
-            })
-        }
-    });
-
-    var signIn = new Vue({
-        el: '#sign-in',
-        data: {
-            username: '',
-            password: '',
-            rememberMe: ''
-        },
-        methods: {
-            submit: function (e) {
-                e.preventDefault();
+    var $ticketsEl = $('#tickets');
+    if ($ticketsEl.length > 0) {
+        var ticketsApp = new Vue({
+            el: '#tickets',
+            data: {
+                tickets: [],
+                error: ''
+            },
+            mounted() {
                 var _this = this;
-
                 $.ajax({
-                    url: '/sign-in.json',
-                    data: {
-                       username: _this.username,
-                       password: _this.password,
-                       "remember-me": _this.rememberMe
-                    },
+                    url: '/tasks.json',
                     method: 'GET',
                     type: 'json',
                     success: function (response) {
-                        if (response) {
-                            console.log(repsonse, 'success');
+                        if (response.result) {
+                            var tickets = response.data;
+                            for (index in tickets) {
+                                var ticket = tickets[index];
+                                console.log(ticket.description)
+                                ticket.description = markdown.toHTML(ticket.description);
+                                _this.tickets.push(tickets[index]);
+                            }
                         }
                     },
                     error: function (response) {
-                        if (response) {
-                            console.log(response, 'error');
+                        if (response.data) {
+                            _this.error = e.data.error;
                         }
                     }
                 });
             }
-        }
-    });
+        });
 
-    var signUpApp = new Vue({
-       el: '#sign-up',
-       data: {
-           username: '',
-           password: '',
-           passwordRepeat: '',
-           errorName: '',
-           errorPass: ''
-       },
-        methods: {
-           submit: function (e) {
-               e.preventDefault();
-               console.log(this.username);
-               console.log(this.password);
-               var _this = this;
-               $.ajax({
-                   //url: 'http://192.168.1.128:7000/login',
-                   url: '/sign-up.json',
-                   data: {
-                       username: _this.username,
-                       password: _this.password,
-                       passwordRepeat: _this.passwordRepeat
-                       /*"remember-me": _this.rememberMe*/
-                   },
-                   method: 'GET',
-                   success: function (e) {
-                       console.log(e, 'success');
-                   },
-                   error: function (e) {
-                       console.log(e, 'error');
-                   }
-               });
-           }
-        }
-    });
+    }
+    var $signInEl = $('#sign-in');
+    if ($signInEl.length > 0) {
+        var signIn = new Vue({
+            el: '#sign-in',
+            data: {
+                username: '',
+                password: '',
+                rememberMe: ''
+            },
+            methods: {
+                submit: function (e) {
+                    e.preventDefault();
+                    var _this = this;
 
+                    $.ajax({
+                        url: '/sign-in.json',
+                        data: {
+                            username: _this.username,
+                            password: _this.password,
+                            "remember-me": _this.rememberMe
+                        },
+                        method: 'GET',
+                        type: 'json',
+                        success: function (response) {
+                            if (response) {
+                                console.log(repsonse, 'success');
+                            }
+                        },
+                        error: function (response) {
+                            if (response) {
+                                console.log(response, 'error');
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+    var $signUpEl = $('#sign-up');
+    if ($signUpEl.length > 0) {
+        var signUpApp = new Vue({
+            el: '#sign-up',
+            data: {
+                username: '',
+                password: '',
+                passwordRepeat: '',
+                errorName: '',
+                errorPass: ''
+            },
+            methods: {
+                submit: function (e) {
+                    e.preventDefault();
+                    console.log(this.username);
+                    console.log(this.password);
+                    var _this = this;
+                    $.ajax({
+                        //url: 'http://192.168.1.128:7000/login',
+                        url: '/sign-up.json',
+                        data: {
+                            username: _this.username,
+                            password: _this.password,
+                            passwordRepeat: _this.passwordRepeat
+                            /*"remember-me": _this.rememberMe*/
+                        },
+                        method: 'GET',
+                        success: function (e) {
+                            console.log(e, 'success');
+                        },
+                        error: function (e) {
+                            console.log(e, 'error');
+                        }
+                    });
+                }
+            }
+        });
+
+    }
 
 
     var app = new Vue({
         el: '#app',
         data: {
-            hello: 'Привет, Vue!'
+            hello: ''
         }
     });
 
-    var usersApp = new Vue({
-        el: '#user',
-        data: {
-            user:
-                { username: 'Саня', karma: '100', about: 'Сражаюсь с криминалом', social_link: 'vk.com' }
-        }
-    });
-    /*    var markdownText = "Hello.\\n\\n* This is markdown.\\n* It is fun\\n* Love it or leave it.";
-        console.log(markdown);
-        var text = markdown.toHTML(markdownText);
-        $('body').append(text);*/
+    var $usersEl = $('#user');
+    if ($usersEl.length > 0) {
+        var usersApp = new Vue({
+            el: '#user',
+            data: {
+                user: ''
+            },
+            mounted() {
+                console.log(this.$el);
+                try {
+                    var userId = getCookie('userId');
+                    if (!userId) {
+                        userId = getUrlParameters("id", "", true);
+                    }
+                } catch (e) {
+                    window.location.href = '/sign-up.html';
+                }
+                var _this = this;
+                $.ajax({
+                    url: '/user.json',
+                    data: {
+                        userId: userId
+                    },
+                    method: 'GET',
+                    type: 'json',
+                    success: function (respond) {
+                        if (respond.result) {
+                            _this.user = respond.data;
+                            console.log(respond, 'success');
+                        }
+                    },
+                    error: function (respond) {
+                        if (respond) {
+                            console.log(respond, 'success');
+                        }
+                    }
+                });
+            }
+        });
+    }
+
 });
 
 function getUrlParameters(parameter, staticURL, decode) {
@@ -148,12 +187,44 @@ function getUrlParameters(parameter, staticURL, decode) {
 var idParameter = getUrlParameters("id", "", true);
 console.log(idParameter);
 
-function bAjax(url, data, method, successCallback, errorCallback) {
-    $.ajax({
-        url: url,
-        data: data,
-        method: method,
-        success: successCallback,
-        error: errorCallback
-    });
+function getCookie(name) {
+    var matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function setCookie(name, value, options) {
+    options = options || {};
+
+    var expires = options.expires;
+
+    if (typeof expires == "number" && expires) {
+        var d = new Date();
+        d.setTime(d.getTime() + expires * 1000);
+        expires = options.expires = d;
+    }
+    if (expires && expires.toUTCString) {
+        options.expires = expires.toUTCString();
+    }
+
+    value = encodeURIComponent(value);
+
+    var updatedCookie = name + "=" + value;
+
+    for (var propName in options) {
+        updatedCookie += "; " + propName;
+        var propValue = options[propName];
+        if (propValue !== true) {
+            updatedCookie += "=" + propValue;
+        }
+    }
+
+    document.cookie = updatedCookie;
+}
+
+function deleteCookie(name) {
+    setCookie(name, "", {
+        expires: -1
+    })
 }
